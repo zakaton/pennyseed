b8ccb5ef2f057aac172292a6030c5cae34ae073e
Zack <zack@ukaton.com>
2022-04-30 07:32:50 -0700

    tailwind prettier plugin

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,37 +2,37 @@ import Head from 'next/head';
 
 function Home() {
   return (
-    <div className="px-8 mt-10">
+    <div className="mt-10 px-8">
       <Head>
         <title>Pennyseed</title>
       </Head>
       <button
         type="button"
-        className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800"
+        className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
       >
         Hello!
       </button>
-      <div className="mx-auto lg:w-3/5 lg:flex lg:flex-row lg:h-auto">
-        <div className="bg-white p-8 rounded-bl-md rounded-br-md lg:rounded-bl-none lg:rounded-tr-md">
-          <h2 className="text-gray-700 font-semibold">
+      <div className="mx-auto lg:flex lg:h-auto lg:w-3/5 lg:flex-row">
+        <div className="rounded-bl-md rounded-br-md bg-white p-8 lg:rounded-bl-none lg:rounded-tr-md">
+          <h2 className="font-semibold text-gray-700">
             Shift overall look and fell by adding these wonderful touches to
             furniture in your home
           </h2>
-          <p className="text-sm text-gray-600 mt-4">
+          <p className="mt-4 text-sm text-gray-600">
             Ever been in a room and felt like something was missing? Perhaps it
             felt slightly bare and uninviting. I&apos;ve got some simple tips to
             help you make any room feel complete.
           </p>
-          <div className="flex items-center mt-8">
+          <div className="mt-8 flex items-center">
             <div className="flex items-center">
               <div className="ml-4">
-                <p className="text-gray-800 text-sm font-semibold">
+                <p className="text-sm font-semibold text-gray-800">
                   Michelle Appleton
                 </p>
-                <p className="text-gray-600 text-sm">28 Jun 2020</p>
+                <p className="text-sm text-gray-600">28 Jun 2020</p>
               </div>
             </div>
-            <div className="w-8 h-8 ml-auto bg-gray-200 rounded-full flex items-center justify-center">
+            <div className="ml-auto flex h-8 w-8 items-center justify-center rounded-full bg-gray-200">
               <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13">
                 <path
                   fill="#6E8098"
