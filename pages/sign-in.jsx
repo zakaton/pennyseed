5e62617303b9b5e5d1e642024b76f765bce1f94d
Zack <zack@ukaton.com>
2022-05-01 18:45:10 -0700

    sign in page

--- pages/sign-in.jsx
+++ pages/sign-in.jsx
@@ -23,21 +23,66 @@ export default function SignIn() {
 
   if (submitted) {
     return (
-      <div>
-        <h1>Check your email to sign in</h1>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Thank you
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Check your email for a confirmation link
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
 
   return (
-    <div>
-      <main>
-        <h1>Sign in</h1>
-        <input onChange={(e) => setEmail(e.target.value)} />
-        <button type="button" onClick={() => signIn()}>
-          sign in
-        </button>
-      </main>
+    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            We'll send a link to your email - no password required
+          </p>
+        </div>
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Email address"
+                onInput={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={(e) => {
+                e.preventDefault();
+                signIn();
+              }}
+            >
+              Send link
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
