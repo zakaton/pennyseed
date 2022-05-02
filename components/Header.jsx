b7adf0e8183d664bf09126e767868edb3d4045b9
Zack <zack@ukaton.com>
2022-05-02 12:01:12 -0700

    update profile icon

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,11 @@
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+import {
+  BellIcon,
+  MenuIcon,
+  XIcon,
+  UserCircleIcon,
+} from '@heroicons/react/outline';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useSession } from '../utils/user-context';
@@ -114,15 +119,10 @@ export default function Header() {
                       <div>
                         <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2">
                           <span className="sr-only">Open user menu</span>
-                          <span className="inline-block h-8 w-8 overflow-hidden rounded-full bg-gray-100">
-                            <svg
-                              className="h-full w-full text-gray-300"
-                              fill="currentColor"
-                              viewBox="0 0 24 24"
-                            >
-                              <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                            </svg>
-                          </span>
+                          <UserCircleIcon
+                            className="h-8 w-8"
+                            aria-hidden="true"
+                          />
                         </Menu.Button>
                       </div>
                       <Transition
