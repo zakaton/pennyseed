b97647e5b14f0e9007b511e4dc60276b36577421
Zack <zack@ukaton.com>
2022-05-02 12:13:25 -0700

    report form validity

--- pages/sign-in.jsx
+++ pages/sign-in.jsx
@@ -52,7 +52,7 @@ export default function SignIn() {
           </p>
         </div>
         <form className="mt-8 space-y-6" action="#" method="POST">
-          <input type="hidden" name="remember" defaultValue="true" />
+          <input type="hidden" name="remember" defaultValue="true" required />
           <div className="-space-y-px rounded-md shadow-sm">
             <div>
               <label htmlFor="email-address" className="sr-only">
@@ -77,6 +77,7 @@ export default function SignIn() {
                 id="agree"
                 name="agree"
                 type="checkbox"
+                required
                 className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                 onInput={(e) => setAgree(e.target.checked)}
               />
@@ -99,6 +100,7 @@ export default function SignIn() {
               className="group relative flex w-full justify-center rounded-md border border-transparent bg-yellow-600 py-2 px-4 text-sm font-medium text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2"
               onClick={(e) => {
                 e.preventDefault();
+                e.target.closest('form').reportValidity();
                 signIn();
               }}
             >
