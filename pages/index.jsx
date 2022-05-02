3ae4e5ad5633014483f4263f739967bc186c46cb
Zack <zack@ukaton.com>
2022-05-02 09:49:48 -0700

    terms of service

--- pages/index.jsx
+++ pages/index.jsx
@@ -32,7 +32,7 @@ export default function Home() {
 
         <p>
           You can check out our{' '}
-          <a href="https://www.pennyseed.fund/campaigns/calculator">
+          <a href="https://www.pennyseed.fund/campaign-calculator">
             Campaign Calculator
           </a>{' '}
           to see how much pledgers would pay given the funding goal and number
@@ -40,23 +40,46 @@ export default function Home() {
         </p>
 
         <p>
-          Payments are done via <a href="https://stripe.com/">Stripe</a>, and
-          pledge amounts include the{' '}
-          <a href="https://stripe.com/pricing">Stripe processing fees</a> (2.9%
-          + $0.30) so the campaigner gets exactly how much they ask for. (so
-          instead of paying $1 you'll actually be paying $1.34, which will be
-          $1.00 after the $0.34 processing fee)
+          Payments are done via{' '}
+          <a href="https://stripe.com/" target="_blank" rel="noreferrer">
+            Stripe
+          </a>
+          , and pledge amounts include the{' '}
+          <a href="https://stripe.com/pricing" target="_blank" rel="noreferrer">
+            Stripe processing fees
+          </a>{' '}
+          (2.9% + $0.30) so the campaigner gets exactly how much they ask for.
+          (so instead of paying $1 you'll actually be paying $1.34, which will
+          be $1.00 after the $0.34 processing fee)
         </p>
 
         <p>
-          This project is hosted on <a href="https://replit.com/">Replit</a> and
-          its source code is{' '}
-          <a href="https://replit.com/@zakaton/Pennyseed">available here</a>.
+          This project is built using{' '}
+          <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+            Next.js
+          </a>{' '}
+          for the frontend,{' '}
+          <a href="https://supabase.com/" target="_blank" rel="noreferrer">
+            Supabase
+          </a>{' '}
+          for user authentication and database management, and is hosted on{' '}
+          <a href="https://vercel.com/" target="_blank" rel="noreferrer">
+            Vercel
+          </a>
+          . The source code is{' '}
+          <a
+            href="https://github.com/zakaton/pennyseed"
+            target="_blank"
+            rel="noreferrer"
+          >
+            available here
+          </a>
+          .
         </p>
 
         <p>
           For more information check out{' '}
-          <a href="https://www.pennyseed.fund/faq">faq</a>.
+          <a href="https://www.pennyseed.fund/faq">FAQ</a>.
         </p>
       </div>
     </>
