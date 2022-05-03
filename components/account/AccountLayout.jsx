97bcf78d288d46292dc5c00c98adc44b94707d96
Zack <zack@ukaton.com>
2022-05-03 09:57:54 -0700

    account settings page

--- components/account/AccountLayout.jsx
+++ components/account/AccountLayout.jsx
@@ -5,17 +5,18 @@ import {
   BellIcon,
 } from '@heroicons/react/outline';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const navigation = [
-  { name: 'General', href: '/account', icon: UserCircleIcon },
+  { name: 'General', hash: '', icon: UserCircleIcon },
   {
     name: 'Payment Info',
-    href: '/account#payment-info',
+    hash: 'payment-info',
     icon: CreditCardIcon,
   },
   {
     name: 'Notifications',
-    href: '/account#notifications',
+    hash: 'notifications',
     icon: BellIcon,
   },
 ];
@@ -26,7 +27,7 @@ function classNames(...classes) {
 
 export default function AccountLayout({ children }) {
   const router = useRouter();
-
+  const hash = router.asPath.split('#')[1] || '';
   return (
     <>
       <Head>
@@ -36,30 +37,30 @@ export default function AccountLayout({ children }) {
         <aside className="px-2 sm:px-6 lg:col-span-3 lg:py-0 lg:px-0">
           <nav className="space-y-1">
             {navigation.map((item) => {
-              const current = router.asPath === item.href;
+              const current = hash === item.hash;
               return (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    current
-                      ? 'bg-gray-50 text-yellow-700 hover:bg-white hover:text-yellow-700'
-                      : 'text-gray-900 hover:bg-gray-50 hover:text-gray-900',
-                    'group flex items-center rounded-md px-3 py-2 text-sm font-medium'
-                  )}
-                  aria-current={current ? 'page' : undefined}
-                >
-                  <item.icon
+                <Link key={item.name} href={`/account#${item.hash}`} passHref>
+                  <a
                     className={classNames(
                       current
-                        ? 'text-yellow-500 group-hover:text-yellow-500'
-                        : 'text-gray-400 group-hover:text-gray-500',
-                      '-ml-1 mr-3 h-6 w-6 flex-shrink-0'
+                        ? 'bg-gray-50 text-yellow-700 hover:bg-white hover:text-yellow-700'
+                        : 'text-gray-900 hover:bg-gray-50 hover:text-gray-900',
+                      'group flex items-center rounded-md px-3 py-2 text-sm font-medium'
                     )}
-                    aria-hidden="true"
-                  />
-                  <span className="truncate">{item.name}</span>
-                </a>
+                    aria-current={current ? 'page' : undefined}
+                  >
+                    <item.icon
+                      className={classNames(
+                        current
+                          ? 'text-yellow-500 group-hover:text-yellow-500'
+                          : 'text-gray-400 group-hover:text-gray-500',
+                        '-ml-1 mr-3 h-6 w-6 flex-shrink-0'
+                      )}
+                      aria-hidden="true"
+                    />
+                    <span className="truncate">{item.name}</span>
+                  </a>
+                </Link>
               );
             })}
           </nav>
--- pages/account.jsx
+++ pages/account.jsx
@@ -1,22 +1,32 @@
 import { useRouter } from 'next/router';
 import { supabase } from '../utils/supabase-client';
 import AccountLayout from '../components/account/AccountLayout';
-import AccountPaymentInfo from '../components/account/AccountPaymentInfo';
 import enforceAuthentication from '../utils/enforce-authentication';
+import AccountPaymentInfo from '../components/account/AccountPaymentInfo';
 import AccountGeneral from '../components/account/AccountGeneral';
 
-const components = {
-  '#payment-info': AccountPaymentInfo,
-  default: AccountGeneral,
-};
+const navigation = [
+  { hash: '', component: AccountGeneral },
+  { hash: 'payment-info', component: AccountPaymentInfo },
+];
+navigation.forEach((item, index) => {
+  // eslint-disable-next-line no-param-reassign
+  item.id = index + 1;
+});
 
 export const getServerSideProps = enforceAuthentication;
 
 export default function Account() {
   const router = useRouter();
-  const hash = router.asPath.split('#')[1];
-  const Component = components[hash] || components.default;
-  return <Component />;
+  const hash = router.asPath.split('#')[1] || '';
+  return navigation.map((item) => {
+    const current = item.hash === hash;
+    return (
+      <div key={item.id} hidden={!current}>
+        <item.component />
+      </div>
+    );
+  });
 }
 
 Account.getNestedLayout = function getNestedLayout(page) {
