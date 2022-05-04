5ea737e29ebedc2d05a26e443cb57bb15af0ec9d
Zack <zack@ukaton.com>
2022-05-04 14:47:41 -0700

    updates

---
+++ pages/api/create-stripe-customer-and-account.js
@@ -0,0 +1,29 @@
+import Stripe from 'stripe';
+import { supabase } from '../../utils/supabase-client';
+
+export default async function handler(req, res) {
+  if (req.query.API_ROUTE_SECRET !== process.env.API_ROUTE_SECRET) {
+    return res.status(401).send('You are not authorized to make this call');
+  }
+
+  const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+  const customer = await stripe.customers.create({
+    email: req.body.record.email,
+  });
+  const account = await stripe.accounts.create({
+    email: req.body.record.email,
+    type: 'express',
+  });
+
+  await supabase
+    .from('profile')
+    .update({
+      stripe_customer: customer.id,
+      stripe_account: account.id,
+    })
+    .eq('id', req.body.record.id);
+
+  res.send({
+    message: `stripe customer ${customer.id} created and account ${account.id} created for id ${req.body.record.id}`,
+  });
+}
---
+++ pages/campaigns/[id].jsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+export default function Campaign() {
+  return (
+    <>
+      <Head>
+        <title>Campaign - Pennyseed</title>
+      </Head>
+      <div className="style-links mx-auto max-w-prose text-lg">
+        <h1>
+          <span className="mt-2 block text-center text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+            Campaign
+          </span>
+        </h1>
+      </div>
+      <div className="style-links prose prose-lg mx-auto mt-6 text-gray-500">
+        lol
+      </div>
+    </>
+  );
+}
