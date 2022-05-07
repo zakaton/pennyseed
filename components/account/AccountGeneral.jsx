b9003761d4470970c88de13b1866000940e69326
Zack <zack@ukaton.com>
2022-05-07 09:19:35 -0700

    Update AccountGeneral.jsx

--- components/account/AccountGeneral.jsx
+++ components/account/AccountGeneral.jsx
@@ -1,13 +1,17 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable camelcase */
 import { useState } from 'react';
 import { useUser } from '../../context/user-context';
 import DeleteAccountModal from './DeleteAccountModal';
 
 export default function AccountGeneral({ isActive }) {
-  const { user } = useUser();
+  const { user, isLoading } = useUser();
   const [showDeleteAccount, setShowDeleteAccount] = useState(false);
 
-  const [didGetStripeAccountInfo, setDidGetStripeAccountInfo] = useState(false);
+  const [didFetchStripeAccountInfo, setDidFetchStripeAccountInfo] =
+    useState(false);
+  const [didReceiveStripeAccountInfo, setDidReceiveStripeAccountInfo] =
+    useState(false);
   const [canCreateCampaigns, setCanCreateCampaigns] = useState(null);
   const [hasCompletedOnboarding, setHasCompletedOnboarding] = useState(null);
   const getStripeAccountInfo = async (override) => {
@@ -22,11 +26,12 @@ export default function AccountGeneral({ isActive }) {
       console.log('has completed onboarding?', has_completed_onboarding);
       setHasCompletedOnboarding(has_completed_onboarding);
     }
+    setDidReceiveStripeAccountInfo(true);
   };
 
-  if (isActive && !didGetStripeAccountInfo) {
+  if (isActive && !didFetchStripeAccountInfo) {
     getStripeAccountInfo();
-    setDidGetStripeAccountInfo(true);
+    setDidFetchStripeAccountInfo(true);
   }
 
   return (
@@ -45,7 +50,7 @@ export default function AccountGeneral({ isActive }) {
               This information is private and will not be shared with anyone
             </p>
           </div>
-          {user && (
+          {!isLoading && user && (
             <div className="mt-5 border-t border-gray-200">
               <dl className="sm:divide-y sm:divide-gray-200">
                 <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
@@ -67,7 +72,11 @@ export default function AccountGeneral({ isActive }) {
                     Can create campaigns?
                   </dt>
                   <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                    {canCreateCampaigns ? 'yes' : 'no'}
+                    {didReceiveStripeAccountInfo
+                      ? canCreateCampaigns
+                        ? 'yes'
+                        : 'no'
+                      : 'loading...'}
                   </dd>
                 </div>
               </dl>
@@ -75,31 +84,34 @@ export default function AccountGeneral({ isActive }) {
           )}
         </div>
         <div className="flex items-end justify-end gap-2 bg-gray-50 px-4 py-3 text-right text-xs sm:px-6 sm:text-sm">
-          {hasCompletedOnboarding ? (
-            <button
-              type="button"
-              onClick={async () => {
-                const response = await fetch('/api/get-stripe-login-link');
-                const { stripe_login_link } = await response.json();
-                window.open(stripe_login_link, '_blank').focus();
-              }}
-              className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
-              Go to Stripe Dashboard
-            </button>
-          ) : (
-            <button
-              type="button"
-              onClick={async () => {
-                const response = await fetch('/api/get-stripe-onboarding-link');
-                const { stripe_onboarding_link } = await response.json();
-                window.open(stripe_onboarding_link, '_blank').focus();
-              }}
-              className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
-              Setup Stripe Account
-            </button>
-          )}
+          {didReceiveStripeAccountInfo &&
+            (hasCompletedOnboarding ? (
+              <button
+                type="button"
+                onClick={async () => {
+                  const response = await fetch('/api/get-stripe-login-link');
+                  const { stripe_login_link } = await response.json();
+                  window.open(stripe_login_link, '_blank').focus();
+                }}
+                className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Go to Stripe Dashboard
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={async () => {
+                  const response = await fetch(
+                    '/api/get-stripe-onboarding-link'
+                  );
+                  const { stripe_onboarding_link } = await response.json();
+                  window.open(stripe_onboarding_link, '_blank').focus();
+                }}
+                className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Setup Stripe Account
+              </button>
+            ))}
 
           <button
             type="button"
