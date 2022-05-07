edbd8de3772b475b661f0291deeeb586fc93d779
Zack <zack@ukaton.com>
2022-05-07 13:57:13 -0700

    Update DeleteAccountModal.jsx

--- components/account/DeleteAccountModal.jsx
+++ components/account/DeleteAccountModal.jsx
@@ -81,7 +81,7 @@ export default function DeleteAccountModal({ open, setOpen }) {
                     onClick={async () => {
                       setIsDeletingAccount(true);
                       await fetch('/api/delete-account');
-                      setIsDeletingAccount(false);
+                      // setIsDeletingAccount(false);
                       signOut();
                     }}
                     className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
