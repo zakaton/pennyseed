6f3307ead566c08289efaddf403c3f4d8d752320
Zack <zack@ukaton.com>
2022-05-07 08:41:33 -0700

    Update delete-account.js

--- pages/api/delete-account.js
+++ pages/api/delete-account.js
@@ -9,17 +9,23 @@ export default async function handler(req, res) {
   }
   console.log('user to delete', user);
 
+  // delete stripe customer and account
+
+  // delete profile
   const deleteProfileResult = await supabase
     .from('profile')
     .delete()
     .eq('id', user.id);
   console.log('delete profile error', deleteProfileResult);
 
+  // delete campaigns
+
+  // delete pledges
+
   const { error: deleteUserError } = await supabase.auth.api.deleteUser(
     user.id
   );
   console.log('delete user error', deleteUserError);
 
-  console.log('deleted user');
-  res.status(200).send('deleted user!');
+  res.status(200).send('deleted user');
 }
