1b059a8f4a34ed99fcffe8988e404ad45fe71695
Zack <zack@ukaton.com>
2022-05-07 08:40:24 -0700

    Update delete-account.js

--- pages/api/delete-account.js
+++ pages/api/delete-account.js
@@ -8,9 +8,18 @@ export default async function handler(req, res) {
     return res.status(401).send('Unauthorized');
   }
   console.log('user to delete', user);
-  await supabase.auth.api.deleteUser(user.id);
+
+  const deleteProfileResult = await supabase
+    .from('profile')
+    .delete()
+    .eq('id', user.id);
+  console.log('delete profile error', deleteProfileResult);
+
+  const { error: deleteUserError } = await supabase.auth.api.deleteUser(
+    user.id
+  );
+  console.log('delete user error', deleteUserError);
+
   console.log('deleted user');
-  await supabase.auth.signOut();
-  console.log('signed out user');
-  res.redirect('/');
+  res.status(200).send('deleted user!');
 }
