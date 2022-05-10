20a76920f2b55e205e5fff356e5407e122bd35ce
Zack <zack@ukaton.com>
2022-05-09 22:42:47 -0700

    fixed rounding issue

--- utils/campaign-calculator.js
+++ utils/campaign-calculator.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-param-reassign */
-export function truncateDollars(value, roundDown = true) {
+export function truncateDollars(value, roundUp = true) {
   value = Number(value);
   value *= 100;
-  value = roundDown ? Math.floor(value) : Math.ceil(value);
+  value = roundUp ? Math.ceil(value) : Math.floor(value);
   value /= 100;
   return value;
 }
@@ -28,30 +28,29 @@ export function getMaximumPossibleNumberOfPledgers(fundingGoal) {
 // https://stripe.com/pricing
 export function getStripeProcessingFee(dollars) {
   dollars = truncateDollars(dollars);
-  const stripeProcessingFee = truncateDollars(dollars * 0.029 + 0.3, false);
+  const stripeProcessingFee = truncateDollars(dollars * 0.029 + 0.3);
   return stripeProcessingFee;
 }
 export function getPennyseedFee(dollars) {
   dollars = truncateDollars(dollars);
-  const pennyseedFee = truncateDollars(dollars * pennyseedFeePercentage, false);
+  const pennyseedFee = truncateDollars(dollars * pennyseedFeePercentage);
   return pennyseedFee;
 }
 export function getAmountAfterProcessing(dollarsBeforeProcessing) {
   const dollars = truncateDollars(dollarsBeforeProcessing);
   const amountAfterProcessing = truncateDollars(
-    dollars - getStripeProcessingFee(dollars),
-    false
+    dollars - getStripeProcessingFee(dollars)
   );
   return amountAfterProcessing;
 }
 export function getAmountBeforeProcessing(dollarsAfterProcessing) {
   const dollars = truncateDollars(dollarsAfterProcessing);
-  return truncateDollars((dollars + 0.3) / (1 - 0.029), false);
+  return truncateDollars((dollars + 0.3) / (1 - 0.029));
 }
 
 export function getPledgeAmount(fundingGoal, numberOfUsers) {
   let pledgeAmount = fundingGoal / numberOfUsers;
-  pledgeAmount = truncateDollars(pledgeAmount, false);
+  pledgeAmount = truncateDollars(pledgeAmount);
   return pledgeAmount;
 }
 export function getPledgeAmountPlusProcessing(fundingGoal, numberOfUsers) {
