94efbd2d548a8b9ca355f9bf679565d371da1df4
Zack <zack@ukaton.com>
2022-05-11 17:14:59 -0700

    Update create-campaign.jsx

--- pages/create-campaign.jsx
+++ pages/create-campaign.jsx
@@ -7,13 +7,6 @@ export default function CreateCampaign() {
       <Head>
         <title>Create Campaign - Pennyseed</title>
       </Head>
-      <div className="style-links mx-auto max-w-prose text-lg">
-        <h1>
-          <span className="mt-2 block text-center text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">
-            Create Campaign
-          </span>
-        </h1>
-      </div>
       <CampaignForm />
     </>
   );
