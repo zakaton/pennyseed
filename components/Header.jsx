4444f51800c0d187577732bd738dbd6de7f230df
Zack <zack@ukaton.com>
2022-05-13 20:13:42 -0700

    fix header bug on mobile

--- components/Header.jsx
+++ components/Header.jsx
@@ -183,7 +183,7 @@ export default function Header() {
                   key={name}
                   href={href}
                   className={classNames(
-                    router.pathname.startsWith(href)
+                    router.pathname === href
                       ? 'border-yellow-500 bg-yellow-50 text-yellow-700'
                       : 'border-transparent  text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700',
                     'block border-l-4 py-2 pl-3 pr-4 text-base font-medium'
