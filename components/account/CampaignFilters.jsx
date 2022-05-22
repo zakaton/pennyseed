c119d93b7bb165ce33dc72c9a489e464aacdb657
Zack <zack@ukaton.com>
2022-05-22 11:39:43 -0700

    compile issues

--- components/account/CampaignFilters.jsx
+++ components/account/CampaignFilters.jsx
@@ -59,12 +59,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-export default function CampaignFilters({
-  filters,
-  setFilters,
-  order,
-  setOrder,
-}) {
+export default function CampaignFilters({ filters, setFilters, setOrder }) {
   const [numberOfActiveFilters, setNumberOfActiveFilters] = useState(0);
   useEffect(() => {
     setNumberOfActiveFilters(Object.keys(filters).length);
@@ -170,7 +165,7 @@ export default function CampaignFilters({
           <div className="mx-auto flex max-w-7xl justify-end px-4 sm:px-6 lg:px-8">
             <Menu as="div" className="relative inline-block">
               <div className="flex">
-                <Menu.Button className="inline-flex group justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+                <Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
                   Sort
                   <ChevronDownIcon
                     className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
