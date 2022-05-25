91621846bc7eac3d4436bd740d3271da76514272
Zack <zack@ukaton.com>
2022-05-25 15:05:58 -0700

    Update enforce-api-route-secret.js

--- utils/enforce-api-route-secret.js
+++ utils/enforce-api-route-secret.js
@@ -1,6 +1,8 @@
 export default function enforceApiRouteSecret(req, res) {
-  console.log(req, process.env.API_ROUTE_SECRET);
-  if (req.query.API_ROUTE_SECRET !== process.env.API_ROUTE_SECRET) {
+  if (
+    (req.query.API_ROUTE_SECRET || req.body.API_ROUTE_SECRET) !==
+    process.env.API_ROUTE_SECRET
+  ) {
     res.status(401).send('You are not authorized to make this call');
     return false;
   }
