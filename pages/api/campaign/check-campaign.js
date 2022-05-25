6efdd3f0c7a58104ce060cd5de8747fcbd77fb6a
Zack <zack@ukaton.com>
2022-05-25 14:47:34 -0700

    Update check-campaign.js

--- pages/api/campaign/check-campaign.js
+++ pages/api/campaign/check-campaign.js
@@ -1,6 +1,29 @@
-/* eslint-disable camelcase */
-/* eslint-disable consistent-return */
+// import Stripe from 'stripe';
+import enforceApiRouteSecret from '../../../utils/enforce-api-route-secret';
+import { getSupabaseService } from '../../../utils/supabase';
 
-export default async function handler(_req, res) {
-  res.status(200).json({ hello: 'world' });
+// const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const supabase = getSupabaseService();
+
+// eslint-disable-next-line consistent-return
+export default async function handler(req, res) {
+  if (!enforceApiRouteSecret(req, res)) {
+    return;
+  }
+
+  // get campaigns that have passed but not processed
+  const {
+    error: getNumberOfCampaignsError,
+    count: numberOfCampaignsToProcess,
+  } = await supabase
+    .from('campaign')
+    .select('*', { count: 'exact' })
+    .match({ approved: true, processed: false })
+    .lte('deadline', new Date().toISOString());
+
+  console.log(getNumberOfCampaignsError, numberOfCampaignsToProcess);
+
+  res.status(200).send({
+    hello: 'world',
+  });
 }
