fbdbcfcba92e429174a64a079a502d6e1fb7e0e2
Zack <zack@ukaton.com>
2022-05-27 08:54:16 -0700

    Update send-email.js

--- utils/send-email.js
+++ utils/send-email.js
@@ -2,11 +2,11 @@ import mail from '@sendgrid/mail';
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-console.log(process.env.SENDGRID_API_KEY);
-
 const adminEmail = 'contact@pennyseed.fund';
 
 export default async function sendEmail(message) {
+  console.log('SENDGRID_API_KEY', process.env.SENDGRID_API_KEY);
+
   return mail.send({
     ...message,
     from: {
