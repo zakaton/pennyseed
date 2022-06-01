a946f2d42dc947d4a87e84e3d462393401aa8ced
Zack <zack@ukaton.com>
2022-05-31 21:12:02 -0700

    Update set-auth-cookie.js

--- pages/api/account/set-auth-cookie.js
+++ pages/api/account/set-auth-cookie.js
@@ -1,26 +1,5 @@
-import { GoTrueClient } from '@supabase/gotrue-js';
-
-const customAuthClient = new GoTrueClient({
-  url: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1`,
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
-    apikey: `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
-  },
-  cookieOptions: {
-    lifetime: 604800,
-    domain: '',
-    path: '/',
-  },
-});
-
-export default (req, res) => {
-  customAuthClient.api.setAuthCookie(req, res);
-};
-
-/*
 import { supabase } from '../../../utils/supabase';
 
 export default async function handler(req, res) {
   await supabase.auth.api.setAuthCookie(req, res);
 }
-*/
