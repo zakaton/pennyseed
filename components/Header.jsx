64587fcb4588c7f652397882877b7b3ad186e82b
Zack <zack@ukaton.com>
2022-06-03 07:57:18 -0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -128,7 +128,8 @@ export default function Header() {
                     </>
                   ) : (
                     <MyLink
-                      href="/sign-in"
+                      href={`/sign-in?redirect_pathname=${router.pathname}`}
+                      as="/sign-in"
                       className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-yellow-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-yellow-700"
                     >
                       Sign in
